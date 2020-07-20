63f126e3f44f5c12eb719451f1f4f7f28535ab00
paulmwatson <github.com@paulmwatson.com>
2020-07-20 16:03:06 +0200

    Fix CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -12,7 +12,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.img_src     :self, "https://#{ENV['AWS_S3_BUCKET_NAME']}.s3.#{ENV['AWS_REGION']}.amazonaws.com/" # , :https, :data
   policy.object_src  :none #
   policy.script_src  :self # , :https
-  policy.style_src   :self # , :https
+  policy.style_src   :self, :unsafe_inline # , :https
   #   # If you are using webpack-dev-server then specify webpack-dev-server host
   policy.connect_src :self # , :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
