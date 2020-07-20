50a3777d326b17f364723b655c3fd5b07365353b
paulmwatson <github.com@paulmwatson.com>
2020-07-20 16:02:22 +0200

    History Stimulus controller

---
+++ app/javascript/controllers/history_controller.js
@@ -0,0 +1,12 @@
+// Send A HREF over XHR GET using Stimulus
+// Usage: <a href="..." data-controller="history" data-action="click->back">Back</a>
+
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  back(e) {
+    if (history.back()) {
+      e.preventDefault()
+    }
+  }
+}
