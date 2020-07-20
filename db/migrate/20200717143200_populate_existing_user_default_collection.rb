4e35ab44e42b06796924dc37d09b61e612f3ca7a
paulmwatson <github.com@paulmwatson.com>
2020-07-20 16:02:50 +0200

    Fix populate migration

--- db/migrate/20200717143200_populate_existing_user_default_collection.rb
+++ db/migrate/20200717143200_populate_existing_user_default_collection.rb
@@ -5,7 +5,6 @@ class PopulateExistingUserDefaultCollection < ActiveRecord::Migration[6.0]
     User.all.each do |user|
       user.collections.create(name: 'Read Later', description: 'A list to read later.')
       user.update(default_collection: user.collections.first)
-      user.default_collection.items << Item.order('RANDOM()').limit(2)
     end
 
     change_column :users, :default_collection_id, :uuid, null: false
